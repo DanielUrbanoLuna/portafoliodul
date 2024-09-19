0491fb68025c247dd904865d21a0cd8eb4adc300
Daniel Urbano <urbanobdn@gmail.com>
2024-09-19 11:36:47 +0200

    pdfuik

--- src/components/DescargarCvPdf.js
+++ src/components/DescargarCvPdf.js
@@ -6,32 +6,22 @@ export const DescargarCvPdf = () => {
   const handleDownloadPDF = () => {
     // Selecciona el contenedor del currículum
     const element = document.querySelector('.curriculum-container');
-
-    // Aplica temporalmente un estilo específico para ajustar el tamaño al formato A4
-    element.style.width = '4.27in'; // Forzamos el ancho a A4 en pulgadas
-    element.style.height = 'auto';  // Permitimos que la altura sea dinámica
-    element.style.padding = '1rem'; // Opcional: Agregamos un padding para márgenes
-
+  
     // Configura las opciones para html2pdf
     const opt = {
-      margin: 0,
+      margin: [0.5, 0.5, 0.5, 0.5], // Margen (arriba, derecha, abajo, izquierda)
       filename: 'Mi_Curriculum.pdf',
       image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 1 }, // Escala 1 para evitar distorsión
+      html2canvas: { scale: 1 }, // Ajustar la escala si es necesario
       jsPDF: { 
         unit: 'in', 
-        format: 'a4', // Formato A4 estándar
+        format: [18, 13], // Tamaño A4 en pulgadas
         orientation: 'portrait' // Vertical
       }
     };
-
+  
     // Genera y descarga el PDF
-    html2pdf().from(element).set(opt).save().then(() => {
-      // Restaurar el estilo original después de la descarga
-      element.style.width = '';
-      element.style.height = '';
-      element.style.padding = '';
-    });
+    html2pdf().from(element).set(opt).save();
   };
 
   return (
